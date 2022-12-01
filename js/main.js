812e7358c8699f756cff76549a4681bcec043e61
Johan Holm <johan_holm@live.com>
2022-12-01 08:14:21 +0100

    Just for show

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// connect to database
+// $conn = mysqli_connect();
+
+// check connection
+// if(!$conn){
+//     echo 'Connetion error: ' . mysqli_connect_error();
+// }
+function getInfo(){
+    var username = document.querySelector("username").value
+    var password = document.querySelector("password").value
+    
+    for
+    if (username == xxxx )
+}
